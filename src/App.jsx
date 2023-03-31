52f8b2ad7b114bdc1384968c668435457b399e25
varuscritical900 <varuscrtical900@gmail.com>
2023-03-31 22:56:16 +0800

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <Router>
+      <Navbar/>
+        <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/home" element={<Home/>}/>
+        <Route exact path="/projects" element={<Projects/>}/>
+        </Routes>
+        <Footer/>
+      </Router>
+
+    {/* <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/home" element={<Home />} />
+        </Routes>
+        <Footer/>
+      </Router> */}
     </div>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link, useLocation } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-warning-subtle">
+  <div className="container-fluid">
+    <Link to="/home" className="navbar-brand"><h1>Marc</h1></Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="navbar-nav">
+        <Link to="/home" className="nav-link active" aria-current="page">Home</Link>
+        <Link to="/projects" className="nav-link"></Link>
+        <Link to="/projects" className="nav-link">Projects</Link>
+      </div>
+    </div>
+  </div>
+</nav>
+  )
+  
+}
+
+export default Navbar
