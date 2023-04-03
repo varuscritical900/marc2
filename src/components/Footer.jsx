a08122984aa319cd3a7275e555a00ce4d304de61
varuscritical900 <varuscrtical900@gmail.com>
2023-04-03 13:09:15 +0800

    fixing some bugs with email reciving api

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
       <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
         <svg className="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
       </a>
-      <span className="mb-3 mb-md-0 text-muted">© Marc James Montero</span>
+      <span className="mb-3 mb-md-0 text-muted">© Marc James Montero | Ph +639466610015</span>
     </div>
 
     <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
         e.preventDefault();
         
     
-        emailjs.sendForm('service_l07lj5o', 'template_y47rose', form.current, 'ISXdOAX4K7EIS7Agw')
+        emailjs.sendForm("service_l07lj5o", "template_y47rose", form.current, "ISXdOAX4K7EIS7Agw")
           .then((result) => {
               console.log(result.text);
               setDone(true);
@@ -20,34 +20,31 @@ const Contact = () => {
   return (
     <div className='container d-flex flex-column-reverse flex-lg-row flex-md-column flex-sm-column-reverse mb-3'>
         {/* Contact */}
-    <div className='container d-flex flex-column  mb-3 mt-5 mx-5'>
+    <div className='container d-flex flex-column  mb-3 mt-5 '>
     <form ref={form} onSubmit={sendEmail} className='d-flex flex-column mb-3'>
     <label>Name</label>
-    <input type="text" name="user_name" placeholder="Your name..." className='w-50'/>
+    <input type="text" name="user_name" placeholder="Your name..." className="user w-100" required/>
     <label>Email</label>
-    <input type="email" name="user_email" className='w-50' placeholder="Your Email..."/>
+    <input type="email" name="user_email" className='user w-100' placeholder="Your Email..." required/>
     <label>Message</label>
-    <textarea name="message" className='w-50' placeholder="Your Message..."/>
-    <input type="submit" value="Send" className='btn btn-warning mt-3 mb-3 w-25'/>
+    <textarea name="message" className='w-100' placeholder="Your Message..." required/>
+    <input type="submit" value="Send" className='btn btn-warning mt-3 mb-3 w-50'/>
     <span>{done && "I have received your message and will respond to you in a timely manner."}</span>
   </form>
   </div>
 
   {/* Carousel */}
-  <div className='container mt-5 mb-3'>
+  <div className=' mt-5 mb-3'>
 
   <div id="carouselExample" class="carousel slide">
   <div className="carousel-inner">
     <div className="carousel-item active">
-      {/* <img src="..." className="d-block w-100" alt="..."> */}
         <img src="https://firebasestorage.googleapis.com/v0/b/chat1-b013f.appspot.com/o/MeMicrosoft.jpg?alt=media&token=b093a049-f326-469b-802d-1c342255fbda"  className="d-block w-100 img-fluid" alt="" />
     </div>
     <div className="carousel-item">
-      {/* <img src="..." className="d-block w-100" alt="..."> */}
       <img src="https://firebasestorage.googleapis.com/v0/b/chat1-b013f.appspot.com/o/Capstone%20Project.png?alt=media&token=e4d74a82-b25a-4500-a99a-1a790b1a13df"  className="d-block w-100" alt="" />
     </div>
     <div className="carousel-item">
-      {/* <img src="..." className="d-block w-100" alt="..."> */}
       <img src="https://firebasestorage.googleapis.com/v0/b/chat1-b013f.appspot.com/o/Project%20Making.png?alt=media&token=9519147f-18b5-4d59-8cad-58fc4738b643"  className="d-block w-100" alt="" />
     </div>
   </div>
