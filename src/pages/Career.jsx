15215562bd2713116f2db413fc207b4016dae6fa
varuscritical900 <varuscrtical900@gmail.com>
2023-06-04 06:03:37 +0800

    some modification

--- src/pages/Career.jsx
+++ src/pages/Career.jsx
@@ -2,111 +2,105 @@ import React from 'react'
 import "../styles/Career.css"
 
 const Career = () => {
-  return (
-<div className="container bootdey mt-2">
-<div className="row gutters">
-<div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
-<div className="card">
-<div className="card-body">
-{/* Timeline start */}
-<div className="timeline">
-	<div className="timeline-row">
-		<div className="timeline-time">
-			2023 - Present<small>May 2023 - Present</small>
-		</div>
-		<div className="timeline-dot fb-bg"></div>
-		<div className="timeline-content">
-			{/* <i className="fa fa-map-marker"></i> */}
-            <i className="fa fa-briefcase"></i>
-			<a href="http://thurstonsoft.com/" target='_blank' class="text-decoration-none text-dark"><h4>Thurston Software Solution Inc.</h4></a>
-            <h5>• Associate Software Developer</h5>
+    return (
+        <div className="container bootdey mt-2">
+            <div className="row gutters">
+                <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                    <div className="card">
+                        <div className="card-body">
+                            {/* Timeline start */}
+                            <div className="timeline">
+                                <div className="timeline-row">
+                                    <div className="timeline-time">
+                                        2023 - Present<small>May 2023 - Present</small>
+                                    </div>
+                                    <div className="timeline-dot fb-bg"></div>
+                                    <div className="timeline-content">
+                                        {/* <i className="fa fa-map-marker"></i> */}
+                                        <i className="fa fa-briefcase"></i>
+                                        <a href="http://thurstonsoft.com/" target='_blank' class="text-decoration-none text-dark"><h4>Thurston Software Solution Inc.</h4></a>
+                                        <h5>• Associate Software Developer</h5>
 
-			<p>As a Associate Software Developer, I hold an entry-level position focused on software development and programming. My primary expertise lies in utilizing technologies such as C#, .NET, MS SQL, and MongoDB. In this role, I am actively involved in various stages of the software development life cycle, working alongside senior developers and team members.</p>
-		</div>
-	</div>
-	<div className="timeline-row">
-		<div className="timeline-time">
-			2022 - 2023<small>June 2022 to May 2023</small>
-		</div>
-		<div className="timeline-dot green-one-bg"></div>
-		<div className="timeline-content green-one">
-			<i className="fa fa-briefcase"></i>
-            <a href="https://www.concentrix.com/" target='_blank' class="text-decoration-none text-dark">
-			<h4>Concentrix</h4></a>
-            <h5>• Customer Service Representative</h5>
-			<p>
-            During my tenure as a Customer Service Representative at Concentrix, I had the opportunity to handle the "TripAdvisor" account, where I provided exceptional assistance to customers who encountered problems with their excursions.
-			</p>
-		</div>
-	</div>
-	<div className="timeline-row">
-		<div className="timeline-time">
-			2020 - 2022<small>September 2020 - January 2022</small>
-		</div>
-		<div className="timeline-dot green-two-bg"></div>
-		<div className="timeline-content green-two">
-			<i className="fa fa-briefcase"></i>
-            <a href="https://www.ibex.co/" target='_blank' class="text-decoration-none text-dark">
-			<h4>IBEX Global</h4></a>
-            <h5>• Customer Service Representative</h5>
-			<p>During my tenure as a Customer Service Representative at IBEX Global, I had the opportunity to handle accounts such as "Sigma Loyalty Group" and "Amazon," where I provided exceptional customer service and support. I consistently demonstrated a customer-centric approach, effective problem-solving skills, and a commitment to providing exceptional support to both the Sigma Loyalty Group and Amazon accounts. 
-            </p>
-		</div>
-	</div>
-	<div className="timeline-row">
-		<div className="timeline-time">
-			2012 - 2020<small>October 2012 - July 2020</small>
-		</div>
-		<div className="timeline-dot green-three-bg"></div>
-		<div className="timeline-content green-three">
-			<i className="fa fa-briefcase"></i>
-			<h4>Manila Massage Centre (Doha, Qatar)</h4>
-            <h5>• Payroll Officer</h5>
-			<p>
-            As a Payroll Officer for 7 years and half at the Manila Massage Center in Doha, Qatar, I played a vital role in ensuring accurate and timely payroll processing for the organization. With a keen eye for detail and a strong understanding of payroll practices and regulations, I efficiently managed the payroll function to ensure employees were compensated correctly and on time.
-			</p>
-		</div>
-	</div>
-	<div className="timeline-row">
-		<div className="timeline-time">
-			2010 - 2011<small>February 11, 2010 - February 16, 2011</small>
-		</div>
-		<div className="timeline-dot green-four-bg"></div>
-		<div className="timeline-content green-four">
-			<i className="fa fa-briefcase"></i>
-			<h4>INNOV8 DATA CENTER INC.</h4>
-            <h5>• Data Encoder</h5>
-			<p className="no-margin">As a Data Encoder at INNOV8 DATA CENTER INC., I played a critical role in accurately and efficiently inputting and managing large volumes of data. With a keen eye for detail and a commitment to data integrity, I contributed to the smooth operation of data management systems within the organization.</p>
-		</div>
-	</div>
-	<div className="timeline-row">
-		<div className="timeline-time">
-			2008 - 2010<small> January 08, 2008 - January 11, 2010
-</small>
-		</div>
-		<div className="timeline-dot teal-bg"></div>
-		<div className="timeline-content teal">
-			<i className="fa fa-briefcase"></i>
-			<h4>Global Info. Quest Strategies INC.</h4>
-            <h5>• Data Encoder</h5>
-			<p className="no-margin">As a Data Encoder at Global Info Quest Strategies INC. from January 08, 2008, to January 11, 2010, I played a crucial role in accurately and efficiently entering and managing data for the organization. With a strong attention to detail and a commitment to data integrity, I contributed to the seamless functioning of data management systems within the company.</p>
-		</div>
-	</div>
-	<div className="timeline-row">
-		<div className="timeline-time">
-			12:30 PM<small>May 25</small>
-		</div>
-	</div>
+                                        <p>As a Associate Software Developer, I hold an entry-level position focused on software development and programming. My primary expertise lies in utilizing technologies such as C#, .NET, MS SQL, and MongoDB. In this role, I am actively involved in various stages of the software development life cycle, working alongside senior developers and team members.</p>
+                                    </div>
+                                </div>
+                                <div className="timeline-row">
+                                    <div className="timeline-time">
+                                        2022 - 2023<small>June 2022 to May 2023</small>
+                                    </div>
+                                    <div className="timeline-dot green-one-bg"></div>
+                                    <div className="timeline-content green-one">
+                                        <i className="fa fa-briefcase"></i>
+                                        <a href="https://www.concentrix.com/" target='_blank' class="text-decoration-none text-dark">
+                                            <h4>Concentrix</h4></a>
+                                        <h5>• Customer Service Representative</h5>
+                                        <p>
+                                            During my tenure as a Customer Service Representative at Concentrix, I had the opportunity to handle the "TripAdvisor" account, where I provided exceptional assistance to customers who encountered problems with their excursions.
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="timeline-row">
+                                    <div className="timeline-time">
+                                        2020 - 2022<small>September 2020 - January 2022</small>
+                                    </div>
+                                    <div className="timeline-dot green-two-bg"></div>
+                                    <div className="timeline-content green-two">
+                                        <i className="fa fa-briefcase"></i>
+                                        <a href="https://www.ibex.co/" target='_blank' class="text-decoration-none text-dark">
+                                            <h4>IBEX Global</h4></a>
+                                        <h5>• Customer Service Representative</h5>
+                                        <p>During my tenure as a Customer Service Representative at IBEX Global, I had the opportunity to handle accounts such as "Sigma Loyalty Group" and "Amazon," where I provided exceptional customer service and support. I consistently demonstrated a customer-centric approach, effective problem-solving skills, and a commitment to providing exceptional support to both the Sigma Loyalty Group and Amazon accounts.
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="timeline-row">
+                                    <div className="timeline-time">
+                                        2012 - 2020<small>October 2012 - July 2020</small>
+                                    </div>
+                                    <div className="timeline-dot green-three-bg"></div>
+                                    <div className="timeline-content green-three">
+                                        <i className="fa fa-briefcase"></i>
+                                        <h4>Manila Massage Centre (Doha, Qatar)</h4>
+                                        <h5>• Payroll Officer</h5>
+                                        <p>
+                                            As a Payroll Officer for 7 years and half at the Manila Massage Center in Doha, Qatar, I played a vital role in ensuring accurate and timely payroll processing for the organization. With a keen eye for detail and a strong understanding of payroll practices and regulations, I efficiently managed the payroll function to ensure employees were compensated correctly and on time.
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="timeline-row">
+                                    <div className="timeline-time">
+                                        2010 - 2011<small>February 11, 2010 - February 16, 2011</small>
+                                    </div>
+                                    <div className="timeline-dot green-four-bg"></div>
+                                    <div className="timeline-content green-four">
+                                        <i className="fa fa-briefcase"></i>
+                                        <h4>INNOV8 DATA CENTER INC.</h4>
+                                        <h5>• Data Encoder</h5>
+                                        <p className="no-margin">As a Data Encoder at INNOV8 DATA CENTER INC., I played a critical role in accurately and efficiently inputting and managing large volumes of data. With a keen eye for detail and a commitment to data integrity, I contributed to the smooth operation of data management systems within the organization.</p>
+                                    </div>
+                                </div>
+                                <div className="timeline-row">
+                                    <div className="timeline-time">
+                                        2008 - 2010<small> January 08, 2008 - January 11, 2010
+                                        </small>
+                                    </div>
+                                    <div className="timeline-dot teal-bg"></div>
+                                    <div className="timeline-content teal">
+                                        <i className="fa fa-briefcase"></i>
+                                        <h4>Global Info. Quest Strategies INC.</h4>
+                                        <h5>• Data Encoder</h5>
+                                        <p className="no-margin">As a Data Encoder at Global Info Quest Strategies INC. from January 08, 2008, to January 11, 2010, I played a crucial role in accurately and efficiently entering and managing data for the organization. With a strong attention to detail and a commitment to data integrity, I contributed to the seamless functioning of data management systems within the company.</p>
+                                    </div>
+                                </div>
 
-
-</div>
-{/* <!-- Timeline end --> */}
-</div>
-</div>
-</div>
-</div>
-</div>
-  )
+                            </div>
+                            {/* <!-- Timeline end --> */}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Career
